b52a837d5ef7c73accd91777a0cc8990432905f0
Jorge Esquiva <jorgeesquivait@gmail.com>
2024-05-18 13:21:32 +0200

    1.7 donde

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,23 +1,43 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const App = () => {
   // guarda los clics de cada botón en su propio estado
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [totalClicks, setTotalClicks] = useState(0);
+  const [averagePuntation, setAveragePuntuation] = useState(0);
+  const [positiveClicks, setPositiveClicks] = useState(0);
+
+  useEffect(() => {
+    if (totalClicks > 0) {
+      const totalPuntuation = good * 1 + neutral * 0 + bad * -1;
+      setAveragePuntuation(totalPuntuation / totalClicks);
+    }
+  });
+
+  useEffect(() => {
+    if (totalClicks > 0) {
+      const positivePuntuation = good;
+      setPositiveClicks((positivePuntuation / totalClicks) * 100);
+    }
+  });
 
   const handleGoodClick = () => {
     setGood(good + 1);
+    setTotalClicks(totalClicks + 1);
     console.log(`Good = ${good}`);
   };
 
   const handleNeutralClick = () => {
     setNeutral(neutral + 1);
+    setTotalClicks(totalClicks + 1);
     console.log(`Neutral = ${neutral}`);
   };
 
   const handleBadClick = () => {
     setBad(bad + 1);
+    setTotalClicks(totalClicks + 1);
     console.log(`Bad = ${bad}`);
   };
 
@@ -31,9 +51,12 @@ const App = () => {
       </div>
       <h2>Statistics</h2>
       <div>
-        <p>Good {good}</p>
-        <p>Neutral {neutral}</p>
-        <p>Bad {bad}</p>
+        <p>Good = {good}</p>
+        <p>Neutral = {neutral}</p>
+        <p>Bad = {bad}</p>
+        <p>All Clicks = {totalClicks}</p>
+        <p>Average = {averagePuntation}</p>
+        <p>Positive = {positiveClicks} %</p>
       </div>
     </div>
   );
