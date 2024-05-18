28d5507788add79c8182415b89f5d284676aba46
Jorge Esquiva <jorgeesquivait@gmail.com>
2024-05-18 14:12:09 +0200

    1.8 done

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,42 +1,26 @@
 import { useEffect, useState } from "react";
+import Statistics from "./Components/statistics";
 
 const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-  const [totalClicks, setTotalClicks] = useState(0);
-  const [averagePuntation, setAveragePuntuation] = useState(0);
-  const [positiveClicks, setPositiveClicks] = useState(0);
-
-  useEffect(() => {
-    if (totalClicks > 0) {
-      const totalPuntuation = good * 1 + neutral * 0 + bad * -1;
-      setAveragePuntuation(totalPuntuation / totalClicks);
-    }
-  });
-
-  useEffect(() => {
-    if (totalClicks > 0) {
-      const positivePuntuation = good;
-      setPositiveClicks((positivePuntuation / totalClicks) * 100);
-    }
-  });
 
   const handleGoodClick = () => {
     setGood(good + 1);
-    setTotalClicks(totalClicks + 1);
+
     console.log(`Good = ${good}`);
   };
 
   const handleNeutralClick = () => {
     setNeutral(neutral + 1);
-    setTotalClicks(totalClicks + 1);
+
     console.log(`Neutral = ${neutral}`);
   };
 
   const handleBadClick = () => {
     setBad(bad + 1);
-    setTotalClicks(totalClicks + 1);
+
     console.log(`Bad = ${bad}`);
   };
 
@@ -49,14 +33,7 @@ const App = () => {
         <button onClick={handleBadClick}>Bad</button>
       </div>
       <h2>Statistics</h2>
-      <div>
-        <p>Good = {good}</p>
-        <p>Neutral = {neutral}</p>
-        <p>Bad = {bad}</p>
-        <p>All Clicks = {totalClicks}</p>
-        <p>Average = {averagePuntation}</p>
-        <p>Positive = {positiveClicks} %</p>
-      </div>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   );
 };
---
+++ part1/unicafe/src/Components/statistics.jsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+
+const Statistics = ({ good, neutral, bad }) => {
+  const [totalClicks, setTotalClicks] = useState(0);
+  const [averagePuntation, setAveragePuntuation] = useState(0);
+  const [positiveClicks, setPositiveClicks] = useState(0);
+
+  useEffect(() => {
+    const total = good + neutral + bad;
+    setTotalClicks(total);
+
+    if (totalClicks > 0) {
+      const totalPuntuation = good - bad;
+      setAveragePuntuation(totalPuntuation / total);
+    }
+  }, [good, neutral, bad]);
+
+  useEffect(() => {
+    if (totalClicks > 0) {
+      const positivePuntuation = good;
+      setPositiveClicks((positivePuntuation / totalClicks) * 100);
+    }
+  }, [totalClicks, good]);
+
+  return (
+    <div>
+      <p>Good = {good}</p>
+      <p>Neutral = {neutral}</p>
+      <p>Bad = {bad}</p>
+      <p>All Clicks = {totalClicks}</p>
+      <p>Average = {averagePuntation}</p>
+      <p>Positive = {positiveClicks} %</p>
+    </div>
+  );
+};
+
+export default Statistics;
