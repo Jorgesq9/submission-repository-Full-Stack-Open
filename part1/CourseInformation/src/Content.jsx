a5b3906386c0659603ac3ab03bd9f7db02ac568c
Jorge Esquiva <jorgeesquivait@gmail.com>
2024-05-18 11:46:19 +0200

    CourseInforamtionDone

---
+++ part1/CourseInformation/src/Content.jsx
@@ -0,0 +1,13 @@
+import Part from "./Part";
+
+const Content = ({ course }) => {
+  return (
+    <div>
+      <Part name={course.parts[0].name} exercises={course.parts[0].exercises} />
+      <Part name={course.parts[1].name} exercises={course.parts[1].exercises} />
+      <Part name={course.parts[2].name} exercises={course.parts[2].exercises} />
+    </div>
+  );
+};
+
+export default Content;
---
+++ part1/CourseInformation/src/Total.jsx
@@ -0,0 +1,9 @@
+const Total = ({ course }) => {
+  const total =
+    course.parts[0].exercises +
+    course.parts[1].exercises +
+    course.parts[2].exercises;
+  return <p>Number of exercises {total}</p>;
+};
+
+export default Total;
